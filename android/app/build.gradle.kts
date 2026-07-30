edf2f820aa11e017cf6ea3dce39c1b8dba401030
Yassin Mohamed Abdalrahman Fadlalla <ym8997914@gmail.com>
2026-07-30 06:54:45 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.zamel_2"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -19,7 +19,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-       targetSdk = 35
+       targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
